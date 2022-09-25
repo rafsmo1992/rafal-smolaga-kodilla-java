bfc4e40d6a3ae4394e671a276f4ab02fd5ed122f
Rafal Smolaga <rafsmo1992@gmail.com>
2022-09-25 19:52:08 +0200

    7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,2 +1,18 @@
-package com.kodilla.stream.array;public interface ArrayOperations {
+package com.kodilla.stream.array;
+
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+
+    static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length)
+                .forEach(number -> System.out.println(numbers[number]));
+
+        double average = IntStream.range(0, numbers.length)
+                .mapToDouble(number -> numbers[number])
+                .average()
+                .orElse(0.00);
+
+        return average;
+    }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,2 +1,17 @@
-package com.kodilla.stream.array;public class ArrayOperationsTestSuite {
+package com.kodilla.stream.array;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage() {
+        //Given
+        int[] numbers = {2, 4, 6, 8, 10};
+        //When
+        double result = ArrayOperations.getAverage(numbers);
+        double expected = 6.0;
+        //Then
+        assertEquals(expected, result);
+    }
 }
