29bf717dea471ccd788a3ed99fe0de7b7f3b82bd
Rafal Smolaga <rafsmo1992@gmail.com>
2022-10-01 18:37:16 +0200

    8.3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,16 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main (String [] args){
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            System.out.println(secondChallenge.probablyIWillThrowException(1.5, 1.5));
+        } catch (Exception e) {
+            System.out.println("It is not correct value " + e);
+        } finally {
+            System.out.println("I am gonna be here... always!");
+        }
+    }
+}
---
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/FileReaderTestSuite.java
@@ -0,0 +1,48 @@
+package com.kodilla.exception.io;
+import com.kodilla.exception.test.SecondChallenge;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileReaderTestSuite {
+
+    @Test
+    void testReadFile() {
+        //given
+        FileReader fileReader = new FileReader();
+        //when & then
+        assertDoesNotThrow(() -> fileReader.readFile());
+    }
+
+    @Test
+    void whenFileDoesntExistsReadFileShouldThrowException() {
+        // given
+        FileReader fileReader = new FileReader();
+        String fileName = "nie_ma_takiego_pliku.txt";
+        // when & then
+        assertThrows(FileReaderException.class, () -> fileReader.readFile(fileName));
+    }
+
+    @Test
+    public void testReadFileWithName() {
+        // given
+        FileReader fileReader = new FileReader();
+        // when & then
+        assertAll(
+                () -> assertThrows(FileReaderException.class, () -> fileReader.readFile("nie_ma_takiego_pliku.txt")),
+                () -> assertThrows(FileReaderException.class, () -> fileReader.readFile(null)),
+                () -> assertDoesNotThrow(() -> fileReader.readFile("names.txt"))
+        );
+    }
+    @Test
+    void testProbablyIWillThrowException (){
+        //Given
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        //When & Then
+        assertAll(
+                ()-> assertThrows(Exception.class, ()->secondChallenge.probablyIWillThrowException(2.0,1.4)),
+                ()-> assertThrows(Exception.class, ()->secondChallenge.probablyIWillThrowException(1.0,1.5))
+        );
+    }
+}
