b5dcdfd363306cf90f386367616530b4249e8a60
Rafal <rafsmo1992@gmail.com>
2023-03-27 06:40:20 +0200

    31.1fixed

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,2 +1,21 @@
-package com.kodilla.testing2.google;public class GoogleTestingApp {
+package com.kodilla.testing2.google;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleTestingApp {
+    public static final String SEARCHFIELD = "_nkw";                       // [1]
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME); // [2]
+        driver.get("https://www.ebay.com/");                                 // [3]
+
+      //  driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div")).click(); // [4]
+
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));      // [5]
+        searchField.sendKeys("Laptop");                                      // [6]
+        searchField.submit();                                                 // [7]
+    }
 }
